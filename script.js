19f3299f99510fb8ae545ed2c50ecdb5c48b43d5
123usef <Yousifm836@gmail.com>
2022-03-26 09:31:55 +0200

    pushhing final changes

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_value").innerText=rateval;
+}
+
+function checkprinc(){
+    var princ = document.getElementById("principal");
+    if(princ.value <= 0)
+        {
+            alert("enter positive number");
+            princ.focus();
+    } 
+        
+}
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100 ; 
+   var year  = new Date().getFullYear() + parseInt(years); 
+   var result = document.getElementById("result");
+   result.innerHTML =
+    "if you deposite " + "<mark>"+principal + "</mark>"+", <br/>" 
+   + "at an interest rate of " + "<mark>"+rate+"</mark>" +"%. <br/>"
+   + "you will receive an aamount of " + "<mark>"+interest+"</mark>"+",<br/>"
+   + "in the year " + "<mark>"+year+"</mark>" ; 
 }
-        
